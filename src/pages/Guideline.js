0e700b932b0ef68022452761839d8c94b7d4d9d9
Osada97 <osadamanohara55@gmail.com>
2021-05-26 10:04:22 +0530

    Chnaged guidlines and features page styles

--- src/pages/Guideline.js
+++ src/pages/Guideline.js
@@ -8,6 +8,10 @@ export default function Guideline() {
         <h1>Our Guidelines</h1>
       </div>
       <div className="main-box-outer">
+        <div className="main-box lang">
+          <h2>Last Update</h2>
+          <p>2021 May 20</p>
+        </div>
         <div className="main-box">
           <h2>
             Eyekon eClass සදහා log වීමේදී "Unable to log in with provided
@@ -19,25 +23,10 @@ export default function Guideline() {
             පැවතීමයි. එම තොරතුරු නිවැරදි දැයි හොදින් පරීක්ෂා කර නැවත වරක් log
             වීමට උත්සාහ කරන්න. එහෙත් එම ගැටලුව පවතීනම්, ඔබගේ දැනට පවතින password
             එක අලුත් password එකක් දක්වා වෙනස් (change/reset) කලයුතු වේ. ඒ සදහා
-            ඔබ Log In බොත්තම අසල ඇති "Forgot Password" මත click කරන්න. නැතහොත්
-            මෙම link එක මත click කරන්න.
-          </p>
-          <p>
-            (https://eyekoninstitute.lk/passwordreset) ඉන්පසු ඔබ Eyekon eClass
-            account එක සැදීමට භාවිත කරන ලද email ලිපිනය ඇතුලත් කරන්න. ඉන්පසු එම
-            email ලිපිනයට password reset link එකක් පැමිණේ. එම link එක click කර
-            ඔබට ඔබගේ password එක change කරගත හැක. ඔබට ඉහත සදහන් කරන ලද ආකාරයට
-            password reset කරගැනීමට නොහැකිනම් පමණක් ඒ සදහා ඔබට Eyekon staff හි
-            සහය ලබාගැනීමට හැකිය. ඒ සදහා පහත සදහන් "Reset My Password" බොත්තම
-            click කර ලැබෙන form එක නිවැරදිව පුරවා SUBMIT” button එක 1වරක් click
-            කරන්න. Password reset කිරීමට ඔබ විසින් ඔබගේ username (class number)
-            නිවැරදිව ලබාදීම අනිවාර්ය වේ. ඔබ විසින් අප වෙත ලබාදෙන ලද username
-            (class number) හි යම් දෝෂයක් පවතීනම් දැනුම්දීමකින් තොරව ඔබගේ පණිවිඩය
-            ප්‍රතික්ෂේප කරනු ලැබේ. අප විසින් ඔබගේ password reset කරන්නේනම් ඒ
-            සදහා සදහා උපරිම පැය 12ක කාලයක් ගතවනු ඇත. ඔබ විසින් ලබාදෙන නව මුරපදය
-            (new password) සුරක්ෂිතව සටහන් කර තබාගන්න. මින් ඉදිරියට ඔබ ඔබගේ
-            student account සදහා log වීමට භාවිත කල යුත්තේ එම නව මුරපදයයි (new
-            password).{" "}
+            ඔබ Log In බොත්තම අසල ඇති "Forgot Password" මත click කරන්න ඉන්පසු ඔබ
+            Eyekon eClass account එක සැදීමට භාවිත කරන ලද email ලිපිනය ඇතුලත්
+            කරන්න. ඉන්පසු එම email ලිපිනයට password reset link එකක් පැමිණේ. එම
+            link එක click කර ඔබට ඔබගේ password එක change කරගත හැක.
           </p>
         </div>
       </div>
