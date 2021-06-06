7db6e27d0e9a9fb7abdd4d5156ea4e0ac0a7490f
Osada97 <osadamanohara55@gmail.com>
2021-06-06 18:40:23 +0530

    Changed Api port

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 // Load dependencies
-const aws = require('aws-sdk');
-const express = require('express');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const cors = require('cors')
+const aws = require("aws-sdk");
+const express = require("express");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const cors = require("cors");
 //const path = require('path');
 const app = express();
 
 // Set S3 endpoint to DigitalOcean Spaces
-const endpoint = 'fra1';
-const spacesEndpoint = new aws.Endpoint('https://'+endpoint+'.digitaloceanspaces.com');
-const mySpace = 'elearn-storage';
+const endpoint = "fra1";
+const spacesEndpoint = new aws.Endpoint(
+  "https://" + endpoint + ".digitaloceanspaces.com"
+);
+const mySpace = "elearn-storage";
 const s3 = new aws.S3({
-    endpoint: spacesEndpoint,
+  endpoint: spacesEndpoint,
 });
-var imageName = '';
+var imageName = "";
 app.use(cors());
 // Change bucket property to your Space name
 const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: mySpace,
-        acl: 'public-read',
-        key: function (request, file, cb) {
-            imageName=file.originalname;
-            cb(null, 'ckeditor/'+file.originalname);
-        }
-    })
-}).array('upload', 1);
+  storage: multerS3({
+    s3: s3,
+    bucket: mySpace,
+    acl: "public-read",
+    key: function (request, file, cb) {
+      imageName = file.originalname;
+      cb(null, "ckeditor/" + file.originalname);
+    },
+  }),
+}).array("upload", 1);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // app.use(express.static(path.join(__dirname, 'build')));
 //
@@ -37,16 +39,16 @@ app.use(express.static('public'));
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-app.post('/uploads', function (request, response, next) {
-    upload(request, response, function (error) {
-        if(error){
-            return console.log(error)
-        }
-        response.status(200).json({
-            uploaded: true,
-            url: `https://${mySpace}.${endpoint}.digitaloceanspaces.com/ckeditor/${imageName}`
-        });
+app.post("/uploads", function (request, response, next) {
+  upload(request, response, function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    response.status(200).json({
+      uploaded: true,
+      url: `https://${mySpace}.${endpoint}.digitaloceanspaces.com/ckeditor/${imageName}`,
     });
+  });
 });
 
 app.listen(8080);
